6d5f93edd1b899bd73d0f6232eec48db03d6191c
aristidesbp <aristidesbp@gmail.com>
2024-09-09 18:38:06 -0300

    Create scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// Exemplo de script customizado, caso necessário
+document.addEventListener('DOMContentLoaded', function() {
+    // Script para destacar a navegação conforme a rolagem
+    const sections = document.querySelectorAll("section");
+    const navLinks = document.querySelectorAll(".nav-link");
+
+    window.addEventListener("
