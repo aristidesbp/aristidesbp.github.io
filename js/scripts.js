d78076a2b0f06e6ef502be1398965717be0e747d
aristidesbp <aristidesbp@gmail.com>
2024-09-20 07:52:15 -0300

    Delete js directory

--- js/scripts.js
+++
@@ -1,7 +0,0 @@
-// Exemplo de script customizado, caso necessário
-document.addEventListener('DOMContentLoaded', function() {
-    // Script para destacar a navegação conforme a rolagem
-    const sections = document.querySelectorAll("section");
-    const navLinks = document.querySelectorAll(".nav-link");
-
-    window.addEventListener("
