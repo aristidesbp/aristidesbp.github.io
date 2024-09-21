1399346f2712a3ce3dc9b92fa0d29a2a54260f0c
aristidesbp <aristidesbp@gmail.com>
2024-09-21 16:17:22 -0300

    Update and rename index_script.js to iscript.js

---
+++ iscript.js
@@ -0,0 +1,37 @@
+// Função de animação do confete
+function startConfetti() {
+    const confettiSound = document.getElementById('confettiSound');
+    confettiSound.play();
+
+    var duration = 5 * 1000;
+    var end = Date.now() + duration;
+
+    (function frame() {
+        confetti({
+            particleCount: 5,
+            angle: 60,
+            spread: 55,
+            origin: { x: 0 }
+        });
+        confetti({
+            particleCount: 5,
+            angle: 120,
+            spread: 55,
+            origin: { x: 1 }
+        });
+
+        if (Date.now() < end) {
+            requestAnimationFrame(frame);
+        }
+    }());
+}
+
+// Ação do botão
+document.getElementById('cta-btn').addEventListener('click', function(event) {
+    event.preventDefault();
+    startConfetti();
+
+    setTimeout(function() {
+        window.location.href = 'home.html';
+    }, 5000); // Espera 5 segundos antes de redirecionar
+});
