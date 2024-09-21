3077fa29469cfd35cc6493f3ceaf21be1688d993
aristidesbp <aristidesbp@gmail.com>
2024-09-21 06:46:11 -0300

    Delete w3scools/layout_interaction.js

--- w3scools/layout_interaction.js
+++
@@ -1,6 +0,0 @@
-// layout_interaction.js
-document.querySelectorAll('.card').forEach(function (card) {
-  card.addEventListener('click', function () {
-    alert('Você clicou em um artigo!');
-  });
-});
