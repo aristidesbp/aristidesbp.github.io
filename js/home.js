350ca6c0d66a3962b8551c0bfc65edc6c69ca9cd
aristidesbp <aristidesbp@gmail.com>
2024-09-28 10:21:33 -0300

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,10 +1,1 @@
-// Função para criar partículas no background
-const numParticles = 4; // Número de partículas
-const particlesContainer = document.querySelector('.particles'); // Seleciona o container de partículas
 
-// Cria as partículas dinamicamente
-for (let i = 0; i < numParticles; i++) {
-    const particle = document.createElement('div'); // Cria um novo div para a partícula
-    particle.classList.add('particle'); // Adiciona a classe de partícula
-    particlesContainer.appendChild(particle); // Adiciona a partícula ao container
-}
