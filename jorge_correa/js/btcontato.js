8119aa21de0383d09414502f1c16af20747d45d2
aristidesbp <aristidesbp@gmail.com>
2024-10-04 15:55:56 -0300

    Update btcontato.js

--- jorge_correa/js/btcontato.js
+++ jorge_correa/js/btcontato.js
@@ -1,17 +1,17 @@
 /* ADICIONE O CODIGO ABAIXAO NO CODIGO QUE DEJA IMPLEMENTAR O BOTÃO DE CONTATO:
 
   <!-- Contêiner para o conteudo carregada dinamicamente -->
-    <div id="btcontato-container"></div>
+    <div id="contact-container"></div>
 */
 
 
 // conteudo.js
 document.addEventListener("DOMContentLoaded", function () {
     // Carregar a nave do arquivo conteudo.html e inserir no contêiner
-    fetch("btcontato.html")
+    fetch("contact.html")
         .then(response => response.text())
         .then(data => {
-            document.getElementById("btcontato-container").innerHTML = data;
+            document.getElementById("contact-container").innerHTML = data;
         })
         .catch(error => console.error('Erro ao carregar o btcontato:', error));
 });
