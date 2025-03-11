862cf343b6763391952d2562ed05902ff1712fdd
aristidesbp <aristidesbp@gmail.com>
2025-03-11 12:52:55 -0300

    Delete js directory

--- js/theme-switcher.js
+++
@@ -1,4 +0,0 @@
-document.querySelector("#theme-toggle").addEventListener("click", () => {
-    document.body.dataset.theme = 
-        document.body.dataset.theme === "dark" ? "light" : "dark";
-});
