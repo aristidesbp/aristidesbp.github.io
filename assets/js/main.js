8c580d1afd4a9bf7d55704ec6f7c5a386b7da5b7
marketindigital <aristidesbp@gmail.com>
2025-04-22 09:43:06 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,45 +1,129 @@
-// arquivo: main.js
-// Importando funções de outros arquivos para serem usadas neste script
-import { navebar } from "./navebar.js";
 import { desafios } from "./desafios.js";
 import { projetos } from "./projetos.js";
 
-// Selecionando elementos importantes do HTML
-const navigation = document.querySelector("#navigation"); // Menu de navegação
-const backToTopButton = document.querySelector("#backToTopButton"); // Botão "Voltar ao Topo"
-const toggle = document.querySelector("#sw-checkbox"); // Botão de alternância de tema (dark/light mode)
-const navbarSection = document.querySelector("#navebar .wrapper"); // Área onde sera exibido a navebar
-const projectsSection = document.querySelector("#projects .wrapper"); // Área onde serão exibidos os projetos
-// Selecionando elementos de animação
+const navigation = document.querySelector("#navigation");
+const backToTopButton = document.querySelector("#backToTopButton");
+const toggle = document.querySelector("#sw-checkbox");
+const projectsSection = document.querySelector("#projects .wrapper");
+
 const notebook_1 = document.querySelector("#notebook-1");
 const notebook_2 = document.querySelector("#notebook-2");
 const notebook_2_white = document.querySelector("#notebook-2-white");
 const vidro = document.querySelector("#vidro");
 
-
-// Evento disparado quando a página termina de carregar
 window.addEventListener("load", function begin() {
-  // Exibe os projetos ao carregar a página
   projetos(projectsSection);
-  
-  // Seleciona o botão para exibir os desafios
   const desafioBtn = document.querySelector("#desafio");
 
-  // Adiciona evento ao botão de desafio
   desafioBtn.addEventListener("click", () => {
-    // Exibe os desafios ao clicar
     desafios(projectsSection);
-    
-    // Adiciona evento para voltar à lista de projetos
     document
       .querySelector("#backToProjectsBtn")
       .addEventListener("click", begin);
   });
 });
 
-
-// Adiciona evento para detectar rolagem da página
 window.addEventListener("scroll", onScroll);
-// Chama a função onScroll para verificar status inicial
 onScroll();
 
+window.onload = setTimeout(() => {
+  notebook_1.style.opacity = 0;
+
+  notebook_1.style.animation = "none";
+  notebook_2.style.animation = "none";
+  notebook_2_white.style.animation = "none";
+  vidro.style.animation = "none";
+}, 4000);
+
+function onScroll() {
+  showNavOnScroll();
+  showBackToTopButtonOnScroll();
+
+  activateMenuAtCurrentSection(about);
+  activateMenuAtCurrentSection(projects);
+  activateMenuAtCurrentSection(knowledge);
+  activateMenuAtCurrentSection(contact);
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2;
+  const sectionTop = section.offsetTop;
+  const sectionHeight = section.offsetHeight;
+
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop;
+  const sectionEndsAt = sectionTop + sectionHeight;
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine;
+
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine;
+
+  const sectionId = section.getAttribute("id");
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`);
+
+  menuElement.classList.remove("active");
+
+  if (sectionBoundaries) {
+    menuElement.classList.add("active");
+  }
+}
+
+function showNavOnScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add("scroll");
+  } else {
+    navigation.classList.remove("scroll");
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 550) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+}
+
+openMenu();
+function openMenu() {
+  const openBtns = document.querySelectorAll(".open");
+  openBtns.forEach((e) => {
+    e.addEventListener("click", () => {
+      document.body.classList.add("menu-expanded");
+    });
+  });
+}
+
+closeMenu();
+function closeMenu() {
+  const closeBtns = document.querySelectorAll(".close");
+  closeBtns.forEach((e) => {
+    e.addEventListener("click", () => {
+      document.body.classList.remove("menu-expanded");
+    });
+  });
+}
+
+ScrollReveal({
+  origin: "bottom",
+  distance: "50px",
+  duration: 1000,
+}).reveal(
+  `#home, 
+  #home img, 
+  #about, 
+  #about header, 
+  #about p,
+  #about img,
+  #projects,
+  #projects header,
+  #projects .card,
+  #knowledge,
+  #knowledg header,
+  #knowledg .card,
+  #contact,
+  #contact header`
+);
+
+toggle.addEventListener("change", () => {
+  document.body.classList.toggle("light-mode");
+});
