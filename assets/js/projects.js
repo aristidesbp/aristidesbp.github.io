a26c76cee6506275452922b892546bda106ec22b
marketindigital <aristidesbp@gmail.com>
2025-04-22 13:31:45 -0300

    Create projects.js

---
+++ assets/js/projects.js
@@ -0,0 +1,16 @@
+function projects(container) {
+  container.innerHTML = `
+    <section id="projects">
+      <div class="wrapper">
+        <header>
+          <h2>Meus Projetos</h2>
+        </header>
+        <div class="content">
+          <p>Em breve, adicionarei aqui alguns dos projetos mais incríveis que já desenvolvi. Fique atento!</p>
+        </div>
+      </div>
+    </section>
+  `;
+}
+
+export { projects };
