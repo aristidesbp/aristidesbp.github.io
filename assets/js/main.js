e30bfd14998148c93399159c7ca96c3fd14b62f9
marketindigital <aristidesbp@gmail.com>
2025-04-22 12:29:25 -0300

    Update main.js
    home adicionado

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,6 +23,13 @@ const navbarSection = document.querySelector("#navbar");
 navbar(navbarSection);
 //</Renderiza a navbar na página>
 
+//<Renderiza a home na página>
+import { home } from "./home.js";
+// Renderiza o componente Home
+const homeContainer = document.querySelector("#home-section");
+home(homeContainer);
+//</Renderiza a home na página>
+
 //<Renderiza a buton-troca-tema na página>
 import { themeToggle } from "./themeToggle.js";
 const themeToggleContainer = document.querySelector("#theme-toggle");
