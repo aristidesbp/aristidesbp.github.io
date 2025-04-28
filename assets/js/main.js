393c26092da4bd79112fc12a907847889f9a3110
marketindigital <aristidesbp@gmail.com>
2025-04-28 08:02:59 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,72 @@
-// arquivo: main.js
+import { desafios } from "./desafios.js";
+import { projetos } from "./projetos.js";
 
-// Adiciona evento para detectar rolagem da página
+const navigation = document.querySelector("#navigation");
+const backToTopButton = document.querySelector("#backToTopButton");
+const toggle = document.querySelector("#sw-checkbox");
+const projectsSection = document.querySelector("#projects .wrapper");
+
+const notebook_1 = document.querySelector("#notebook-1");
+const notebook_2 = document.querySelector("#notebook-2");
+const notebook_2_white = document.querySelector("#notebook-2-white");
+const vidro = document.querySelector("#vidro");
+
+window.addEventListener("load", function begin() {
+  projetos(projectsSection);
+  const desafioBtn = document.querySelector("#desafio");
+
+  desafioBtn.addEventListener("click", () => {
+    desafios(projectsSection);
+    document
+      .querySelector("#backToProjectsBtn")
+      .addEventListener("click", begin);
+  });
+});
+
+window.addEventListener("scroll", onScroll);
+onScroll();
+
+window.onload = setTimeout(() => {
+  notebook_1.style.opacity = 0;
+
+  notebook_1.style.animation = "none";
+  notebook_2.style.animation = "none";
+  notebook_2_white.style.animation = "none";
+  vidro.style.animation = "none";
+}, 4000);
+
+function onScroll() {
+  showNavOnScroll();
+  showBackToTopButtonOnScroll();
+
+  activateMenuAtCurrentSection(about);
+  activateMenuAtCurrentSection(projects);
+  activateMenuAtCurrentSection(knowledge);
+  activateMenuAtCurrentSection(contact);
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2;
+  const sectionTop = section.offsetTop;
+  const sectionHeight = section.offsetHeight;
+
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop;
+  const sectionEndsAt = sectionTop + sectionHeight;
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine;
+
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine;
+
+  const sectionId = section.getAttribute("id");
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`);
+
+  menuElement.classList.remove("active");
+
+  if (sectionBoundaries) {
+    menuElement.classList.add("active");
+  }
+}
+
 function showNavOnScroll() {
   if (scrollY > 0) {
     navigation.classList.add("scroll");
@@ -9,7 +75,6 @@ function showNavOnScroll() {
   }
 }
 
-// Adiciona evento voltar-topo
 function showBackToTopButtonOnScroll() {
   if (scrollY > 550) {
     backToTopButton.classList.add("show");
@@ -18,51 +83,47 @@ function showBackToTopButtonOnScroll() {
   }
 }
 
-//<Renderiza a navbar na página>
-import { navbar } from "./navbar.js";
-const navbarSection = document.querySelector("#navbar");
-navbar(navbarSection);
-//</Renderiza a navbar na página>
+openMenu();
+function openMenu() {
+  const openBtns = document.querySelectorAll(".open");
+  openBtns.forEach((e) => {
+    e.addEventListener("click", () => {
+      document.body.classList.add("menu-expanded");
+    });
+  });
+}
 
-//<Renderiza a home na página>
-import { home } from "./home.js";
-// Renderiza o componente Home
-const homeContainer = document.querySelector("#home-section");
-home(homeContainer);
-//</Renderiza a home na página>
+closeMenu();
+function closeMenu() {
+  const closeBtns = document.querySelectorAll(".close");
+  closeBtns.forEach((e) => {
+    e.addEventListener("click", () => {
+      document.body.classList.remove("menu-expanded");
+    });
+  });
+}
 
-//<Renderiza a contact na página>
-import { contact } from "./contact.js";
-const contactContainer = document.querySelector("#contact-section");
-contact(contactContainer);
-//</Renderiza a contact na página>
+ScrollReveal({
+  origin: "bottom",
+  distance: "50px",
+  duration: 1000,
+}).reveal(
+  `#home, 
+  #home img, 
+  #about, 
+  #about header, 
+  #about p,
+  #about img,
+  #projects,
+  #projects header,
+  #projects .card,
+  #knowledge,
+  #knowledg header,
+  #knowledg .card,
+  #contact,
+  #contact header`
+);
 
-//<Renderiza o sobre-mim na página>
-import { aboutMe } from "./aboutMe.js";
-const aboutMeContainer = document.querySelector("#about-section");
-aboutMe(aboutMeContainer);
-//</Renderiza o sobre-mim na página>
-
-
-// Renderiza o componente Conhecimentos
-import { knowledge } from "./knowledge.js";
-const knowledgeContainer = document.querySelector("#knowledge-section");
-knowledge(knowledgeContainer);
-
-
-// Renderiza o componente Projetos
-import { projects } from "./projects.js";
-const projectsContainer = document.querySelector("#projects-section");
-projects(projectsContainer);
-
-// Renderiza o componente Footer
-import { footer } from "./footer.js";
-const footerContainer = document.querySelector("#footer-section");
-footer(footerContainer);
-
-
-//<Renderiza a buton-troca-tema na página>
-import { themeToggle } from "./themeToggle.js";
-const themeToggleContainer = document.querySelector("#theme-toggle");
-themeToggle(themeToggleContainer);
-//</Renderiza a buton-troca-tema na página>
+toggle.addEventListener("change", () => {
+  document.body.classList.toggle("light-mode");
+});
