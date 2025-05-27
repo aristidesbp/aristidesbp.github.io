cc0ade326bf089a7cc682f120e99bb46f0b3ecee
marketindigital <aristidesbp@gmail.com>
2025-05-27 13:09:41 -0300

    Update model.js
    model

--- projetos/abp-site/assets/js/model.js
+++ projetos/abp-site/assets/js/model.js
@@ -1,1 +1,13 @@
-model.js 
+// assets/js/model.js
+
+export const portfolioData = {
+  sobre: "Sou Aristides, desenvolvedor Front-end e gestor de tráfego pago. Trabalho com soluções inteligentes e foco em resultado.",
+  projetos: [
+    { nome: "Projeto Coworking", link: "https://github.com/..." },
+    { nome: "App Lista de Tarefas", link: "https://github.com/..." }
+  ],
+  contato: {
+    email: "aristides@example.com"
+  }
+};
+
