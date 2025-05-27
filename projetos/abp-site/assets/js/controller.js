d430decb4897b7ae68b833fa2f49a06c33cb41dd
marketindigital <aristidesbp@gmail.com>
2025-05-27 13:10:27 -0300

    Update controller.js

--- projetos/abp-site/assets/js/controller.js
+++ projetos/abp-site/assets/js/controller.js
@@ -1,1 +1,9 @@
 
+// assets/js/controller.js
+import { loadHeader, renderPortfolio } from "./view.js"; // importa funções da View
+import { portfolioData } from "./model.js";              // importa dados do Model
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadHeader();                     // carrega o cabeçalho via DOM
+  renderPortfolio(portfolioData);  // renderiza os dados no corpo da página
+});
