297c0ed27035f4c5e72c03e32982b3c60e7c82de
marketindigital <aristidesbp@gmail.com>
2026-01-30 16:43:11 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,3 @@
-const dbsupabase = supabase.createClient(
-  'SUA_URL_AQUI', 
-  'SUA_KEY_AQUI'
-)
+
+const supabase_url = 'https://jxvrnzodkahspmyruhdm.supabase.co';
+const supabase_nanon + 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp4dnJuem9ka2Foc3BteXJ1aGRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk3Njg2ODgsImV4cCI6MjA4NTM0NDY4OH0.H2NvKCv7A2vLYY9BZF5nl68XocHUZKLMxeh6b1v_4SE';
