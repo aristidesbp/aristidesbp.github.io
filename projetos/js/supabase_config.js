f19dfd493c51b89841093c1e824a123f08b7dd50
marketindigital <aristidesbp@gmail.com>
2026-02-02 02:28:30 -0300

    Update supabase_config.js

--- projetos/js/supabase_config.js
+++ projetos/js/supabase_config.js
@@ -1,3 +1,3 @@
-const supabase_url = 'https://tefvegltvqwvggspxaiw.supabase.co';
+const supabase_url = 'https://jiaautjsmiosovunjcjc.supabase.co';
 
-const supabase_anonkey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRlZnZlZ2x0dnF3dmdnc3B4YWl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk5NjY2NjksImV4cCI6MjA4NTU0MjY2OX0.9eNliDpzKt8Thq8NjHrHouEiAJ3KUAaKg4wi4j9TlNc';
+const supabase_anonkey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImppYWF1dGpzbWlvc292dW5qY2pjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAwMDEwOTMsImV4cCI6MjA4NTU3NzA5M30.LFGOwXTu2vQQzUtStXZPKuH7dnk2g5yVqab1GTIVnfY';
