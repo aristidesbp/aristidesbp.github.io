b46cd6cfa19b720fc4b2509ee1af4b8478a2ffb0
marketindigital <aristidesbp@gmail.com>
2026-02-03 15:54:54 +0000

    limpando repositorio

--- assets/usuarios/gatilho.sql
+++
@@ -1,6 +0,0 @@
--- Remove se já existir para evitar erro de duplicidade
-DROP TRIGGER IF EXISTS on_auth_user_created ON auth.users;
-
-CREATE TRIGGER on_auth_user_created
-  AFTER INSERT ON auth.users
-  FOR EACH ROW EXECUTE PROCEDURE public.handle_new_user();
