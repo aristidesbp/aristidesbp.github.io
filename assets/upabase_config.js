a466f8ec823155e3e2c33b6a6d9d2d6838a8edc0
marketindigital <aristidesbp@gmail.com>
2026-02-03 12:00:52 -0300

    Update upabase_config.js

--- assets/upabase_config.js
+++ assets/upabase_config.js
@@ -4,7 +4,7 @@
  */
 
 const SUPABASE_URL = 'https://luanoblszirvzqidhecr.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_jqhEnUuuEF4-zxKbgupuTQ_n-7h-qTp';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx1YW5vYmxzemlydnpxaWRoZWNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAxMTEzMTgsImV4cCI6MjA4NTY4NzMxOH0.9clHxZCsq9dVx_DTwjNER6GIe7bla3QGKUOWZukWrBo';
 
 // Inicializa o cliente Supabase globalmente
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
