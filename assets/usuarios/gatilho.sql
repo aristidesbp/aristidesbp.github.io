93ea3ff815b0a33d41f1c4949be773a710f17c97
marketindigital <aristidesbp@gmail.com>
2026-02-03 11:19:13 -0300

    Create gatilho.sql

---
+++ assets/usuarios/gatilho.sql
@@ -0,0 +1,6 @@
+-- Remove se já existir para evitar erro de duplicidade
+DROP TRIGGER IF EXISTS on_auth_user_created ON auth.users;
+
+CREATE TRIGGER on_auth_user_created
+  AFTER INSERT ON auth.users
+  FOR EACH ROW EXECUTE PROCEDURE public.handle_new_user();
