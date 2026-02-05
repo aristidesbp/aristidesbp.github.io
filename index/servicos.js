0b8f4065d85476f3ef997147325aa204196082fd
Aristidesbp <aristidesbp@gmail.com>
2026-02-05 13:57:36 -0300

    DESCRIÇÃO_DA_ALTERACAO

--- index/servicos.js
+++ index/servicos.js
@@ -50,7 +50,7 @@ A diferença entre empresas que crescem e empresas que sobrevivem está na estra
 
   container.innerHTML = `
     <div class="servicos-wrapper">
- 
+    
       ${servicos.map((s, i) => `
         <div class="card-servico" data-index="${i}">
           <h3>${s.titulo}</h3>
