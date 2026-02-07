0ad95b0c9ccf977cb3cb5bde10f38b21407968e3
marketindigital <aristidesbp@gmail.com>
2026-02-07 07:57:29 -0300

    Update player_logic.js

--- assets/musicas/player_logic.js
+++ assets/musicas/player_logic.js
@@ -1,8 +1,25 @@
 // Liste aqui os nomes exatos dos arquivos que estão na pasta /musicas
 const musicas = [
-    "musica1.mp3",
-    "musica2.mp3",
-    "exemplo-track.mp3"
+    "01.mp3",
+    "02.mp3",
+     "03.mp3",
+    "04.mp3",
+     "05.mp3",
+    "06.mp3",
+     "07.mp3",
+    "08.mp3",
+     "09.mp3",
+    "10.mp3",
+     "11.mp3",
+    "12.mp3",
+     "13.mp3",
+    "14.mp3",
+     "15.mp3",
+    "16.mp3",
+     "17.mp3",
+    "18.mp3",
+     "19.mp3",
+       "20.mp3"
 ];
 
 const playlistElement = document.getElementById('playlist');
