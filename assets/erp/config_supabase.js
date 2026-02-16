848dceda55da2ef89e7a64b65add2112ec5ab3a2
marketindigital <aristidesbp@gmail.com>
2026-02-16 16:01:13 -0300

    Create config_supabase.js

---
+++ assets/erp/config_supabase.js
@@ -0,0 +1,8 @@
+// Configuração do Supabase (Mantenha suas chaves conforme o arquivo original)
+const supabaseUrl = 'https://eisruaetsqrratemqswv.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpc3J1YWV0c3FycmF0ZW1xc3d2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk4MDI4OTAsImV4cCI6MjA4NTM3ODg5MH0.Rb-nu9zBL7TNWoGNYHvETWMfbqO1NF7UID4TdSYyKS4';
+const _supabase = supabase.createClient(supabaseUrl, supabaseKey);
+window.supabaseClient = _supabase;
+
+let html5QrCode;
+
