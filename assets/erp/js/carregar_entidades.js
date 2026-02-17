7a7c45aed918a749c7dbd9a28a7aa1019a252ea5
marketindigital <aristidesbp@gmail.com>
2026-02-17 09:54:55 -0300

    Update and rename carrega_rentidades.js to carregar_entidades.js

--- assets/erp/js/carregar_entidades.js
+++ assets/erp/js/carregar_entidades.js
@@ -1,4 +1,11 @@
-// carrega_rentidades.js
+/* carregar_entidades.js
+Elemento de formulario para selecionar usuario na tabela entidades
+
+ EXEMPLO DE COMO USAR NO HTML:
+  <div><label>Entidade</label><select id="entidade_id"><option value="">Selecione...</option></select></div>
+  <script src="js/carregar_entidades.js"></script>
+
+*/
 async function carregarEntidades() {
     const { data, error } = await _supabase.from('entidades').select('id, nome_completo');
     if (!error) {
