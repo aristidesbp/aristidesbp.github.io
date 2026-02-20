3e168d86f2ca7203aed802a18caab38065e25106
marketindigital <aristidesbp@gmail.com>
2026-02-20 09:23:52 -0300

    Delete assets/app directory

--- assets/app/tarefas/deletar_tarefas.js
+++
@@ -1,7 +0,0 @@
-async function deleteTask(id) {
-    if (confirm("Excluir tarefa?")) {
-        await window.supabaseClient.from('tarefas').delete().eq('id', id);
-        location.reload();
-    }
-}
-
--- assets/app/tarefas/limpar_tarefas.js
+++
@@ -1,9 +0,0 @@
-function resetForm() {
-    const fields = ['task-id', 'title', 'content', 'entidade', 'status', 'observacao'];
-    fields.forEach(id => {
-        const el = document.getElementById(id);
-        if(el) el.value = (id === 'status' ? 'pendente' : '');
-    });
-    document.getElementById('btn-save').innerHTML = `<span class="material-symbols-outlined">save</span> Salvar Tarefa`;
-}
-
