6d186dd3d621cfd1df1dc9dc7b2446b0bb334cac
marketindigital <aristidesbp@gmail.com>
2026-02-27 14:20:23 -0300

    Create entidades.js

---
+++ assets/erp/src/view/entidades.js
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Gestão de Entidades - ERP ABP</title>
+
+  <!-- Estilos -->
+  <script src="https://cdn.tailwindcss.com"></script>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="../assets/style/style_entidades.css">
+  <link rel="stylesheet" href="../assets/style/style_navbar.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+</head>
+
+<body class="font-inter">
+
+  <!-- Navbar -->
+  <div id="navbar"></div>
+
+  <!-- Conteúdo da página -->
+  <div id="form_entidades"></div>
+  <div id="lista_entidades"></div>
+
+  <!-- Controller -->
+  <script src="../controllers/EntidadeController.js"></script>
+
+</body>
+</html>
