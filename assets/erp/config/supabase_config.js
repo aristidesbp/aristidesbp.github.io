6dc8e95a9a6262087a36e89f87ef0b060a96d8ac
marketindigital <aristidesbp@gmail.com>
2026-03-02 17:15:27 -0300

    Update supabase_config.js

--- assets/erp/config/supabase_config.js
+++ assets/erp/config/supabase_config.js
@@ -1,9 +1,10 @@
 
-// SUPABASE_CONFIG.JS
-const supabaseUrl = 'https://eisruaetsqrratemqswv.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpc3J1YWV0c3FycmF0ZW1xc3d2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk4MDI4OTAsImV4cCI6MjA4NTM3ODg5MH0.Rb-nu9zBL7TNWoGNYHvETWMfbqO1NF7UID4TdSYyKS4';
+// NO SETINS DO PROJETO 
+//Data API:
+const supabaseUrl = 'https://vrsiiwuifapobeiguldf.supabase.co';
+// Legacy anon, service_role API keys:
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZyc2lpd3VpZmFwb2JlaWd1bGRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0NjM0MTgsImV4cCI6MjA4ODAzOTQxOH0.F5eLwudxDdlRrQJF2IEJ_P5YoaKL98_qSkgr8faots0';
 // Inicializa o cliente Supabase
 const _supabase = supabase.createClient(supabaseUrl, supabaseKey);
 // Exporta para ser usado em outros scripts
 window.supabaseClient = _supabase; 
-
