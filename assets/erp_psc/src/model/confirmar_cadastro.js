75fdd2f23db59f5a99dd0d304ef8f73fb3e85338
marketindigital <aristidesbp@gmail.com>
2026-03-04 12:30:25 -0300

    Delete assets/erp_psc/src/model/confirmar_cadastro.js

--- assets/erp_psc/src/model/confirmar_cadastro.js
+++
@@ -1,8 +0,0 @@
-/* Função de alerta confirmando se é para realmente cadastrar */
-function confirmarCadastro() {
-    const email = document.getElementById('email').value;
-    if (!email) return alert("Digite um e-mail!");  
-    if (confirm(`Deseja criar uma conta para: ${email}?`)) {
-        realizarCadastro(); 
-    }
-}
