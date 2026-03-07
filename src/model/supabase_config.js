33e1e8c1694f9b42556e73733b534a2d796058bd
marketindigital <aristidesbp@gmail.com>
2026-03-07 11:02:04 -0300

    Update supabase_config.js

--- src/model/supabase_config.js
+++ src/model/supabase_config.js
@@ -1,9 +1,8 @@
 
 // SUPABASE_CONFIG.JS
-const supabaseUrl = 'https://eisruaetsqrratemqswv.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpc3J1YWV0c3FycmF0ZW1xc3d2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk4MDI4OTAsImV4cCI6MjA4NTM3ODg5MH0.Rb-nu9zBL7TNWoGNYHvETWMfbqO1NF7UID4TdSYyKS4';
+const supabaseUrl = 'https://rdcfehshnzjkwajxdmnb.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkY2ZlaHNobnpqa3dhanhkbW5iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4ODMwNzEsImV4cCI6MjA4ODQ1OTA3MX0.aEsdWG4EvndJ0j_1zkTQNeQDUyZ6VMiviRH4QIKlR14';
 // Inicializa o cliente Supabase
 const _supabase = supabase.createClient(supabaseUrl, supabaseKey);
 // Exporta para ser usado em outros scripts
 window.supabaseClient = _supabase; 
-
