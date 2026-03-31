04e77577683343c0667124486aea7239ce869269
marketindigital <aristidesbp@gmail.com>
2026-03-31 00:18:20 -0300

    Update Supabase URL and key in configuration

--- src/model/supabase_config.js
+++ src/model/supabase_config.js
@@ -1,8 +1,8 @@
 // src/model/supabase_config.js
 
 // Dados do painel do Supabase (Project Settings > API)
-const supabaseUrl = 'https://slfwwtokaktjrorotajh.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNsZnd3dG9rYWt0anJvcm90YWpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ0MDI0NzYsImV4cCI6MjA4OTk3ODQ3Nn0.Ji2Y7Hg3cj6MAHDhQcZp8eFGrIVoA-5RzX6wZ7d4HU8';
+const supabaseUrl = 'https://flbcktmujsjxukmtqriq.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsYmNrdG11anNqeHVrbXRxcmlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4ODA3NjIsImV4cCI6MjA5MDQ1Njc2Mn0.kigBNnujASjlkFcm5xGOqauz6zrgFGVu_rdQJuJkxnE';
 
 // Inicializa o cliente globalmente
 const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
