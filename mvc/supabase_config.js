4c6885210705f7011c88aead18a406792a9c8e5e
marketindigital <aristidesbp@gmail.com>
2026-04-02 14:44:56 -0300

    Create supabase_config.js

---
+++ mvc/supabase_config.js
@@ -0,0 +1,5 @@
+// Configuração do Supabase (Atenção: substitui a chave abaixo pela tua verdadeira)
+        const supabaseUrl = 'https://xjmsksrhdedwrlanpmmi.supabase.co';
+        const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhqbXNrc3JoZGVkd3JsYW5wbW1pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUwNDE5ODQsImV4cCI6MjA5MDYxNzk4NH0.X2S4UZ3WGLoxx9LsNNbJ6-kyM0DAQoTr8wY57O6m4ZA'; 
+        const _supabase = supabase.createClient(supabaseUrl, supabaseKey);
+
