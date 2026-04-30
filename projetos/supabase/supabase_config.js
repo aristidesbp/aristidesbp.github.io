f6cbd89e5db820a4ed9ae26be47bbbaaea56b0d0
marketindigital <aristidesbp@gmail.com>
2026-04-30 08:50:59 -0300

    Update supabase_config.js

--- projetos/supabase/supabase_config.js
+++ projetos/supabase/supabase_config.js
@@ -1,5 +1,4 @@
 // 1. Chaves de Acesso (Substitua pelas suas)
-const SUPABASE_URL = 'https://topvecgoiypaqzaemkgn.supabase.co/rest/v1/';
+const SUPABASE_URL = 'https://topvecgoiypaqzaemkgn.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRvcHZlY2dvaXlwYXF6YWVta2duIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc1NDc4NTEsImV4cCI6MjA5MzEyMzg1MX0.WBu5Tl4WdpuJ183w08hVJ_BVkKsPKf_qkjwuIchUV-g';
 console.log("✅ supabase_config conectado com sucesso!.");
-
