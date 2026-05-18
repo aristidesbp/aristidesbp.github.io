39c7cdb3d4707d33bf9dd0b74be2d32d03c4359e
marketindigital <aristidesbp@gmail.com>
2026-05-18 11:34:47 -0300

    Update supabase_config.js

--- supabase_config.js
+++ supabase_config.js
@@ -4,4 +4,4 @@ const SUPABASE_ANON_KEY = 'api_key';
 const SUPABASE_URL = 'data_api';
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_key);
 
-console.log("✅ conectado com o arquivo supabase/supabase_config.js");
+
