81c2ef1f2071280b476353f2a402428d8cdeabb1
Aristidesbp <aristidesbp@gmail.com>
2026-06-24 11:24:00 -0300

    Revise Supabase configuration constants
    Updated Supabase configuration with new URL and key.

--- assets/js/supabase_config.js
+++ assets/js/supabase_config.js
@@ -1,9 +1,5 @@
-
-// SUPABASE_CONFIG.JS
-const supabaseUrl = 'https://eisruaetsqrratemqswv.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpc3J1YWV0c3FycmF0ZW1xc3d2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk4MDI4OTAsImV4cCI6MjA4NTM3ODg5MH0.Rb-nu9zBL7TNWoGNYHvETWMfbqO1NF7UID4TdSYyKS4';
-// Inicializa o cliente Supabase
-const _supabase = supabase.createClient(supabaseUrl, supabaseKey);
-// Exporta para ser usado em outros scripts
-window.supabaseClient = _supabase; 
-
+// assets/js/supabase_config.js
+// criar constantes com chave de acesso.
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhqZHZrdnJjcXR4enlkanZlZ2N6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyNzk0MDAsImV4cCI6MjA5Nzg1NTQwMH0.CTNUNyEXcAwySQy2Rd9Txw3XRX-tR5jgy51FhkvCLRE';
+const SUPABASE_URL = 'https://hjdvkvrcqtxzydjvegcz.supabase.co/rest/v1/';
+console.log("✅ supabase_config.js");
