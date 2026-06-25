ccaad85060d9f3fd32ea3eeded5c058cd05931f5
Aristidesbp <aristidesbp@gmail.com>
2026-06-25 08:18:08 -0300

    Update Supabase URL and API key

--- supabase_config.js
+++ supabase_config.js
@@ -1,9 +1,9 @@
 
 // NO SETINS DO PROJETO 
 //Data API:
-const supabaseUrl = 'https://vrsiiwuifapobeiguldf.supabase.co';
+const supabaseUrl = 'https://ewbymweduemrufejwzwh.supabase.co';
 // Legacy anon, service_role API keys:
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZyc2lpd3VpZmFwb2JlaWd1bGRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0NjM0MTgsImV4cCI6MjA4ODAzOTQxOH0.F5eLwudxDdlRrQJF2IEJ_P5YoaKL98_qSkgr8faots0';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV3Ynltd2VkdWVtcnVmZWp3endoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIzMDg2NjUsImV4cCI6MjA5Nzg4NDY2NX0.5UP1OJg9INBoLL6STSe-Gh33N7Gmt0B7bcU-mgwpFIw';
 // Inicializa o cliente Supabase
 const _supabase = supabase.createClient(supabaseUrl, supabaseKey);
 // Exporta para ser usado em outros scripts
