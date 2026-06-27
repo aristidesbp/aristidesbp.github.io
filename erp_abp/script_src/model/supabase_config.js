a1e9518488f2d18841afa869bd6e7a3a7171873f
Aristidesbp <aristidesbp@gmail.com>
2026-06-27 05:00:56 -0300

    Update Supabase URL and API key in configuration

--- erp_abp/script_src/model/supabase_config.js
+++ erp_abp/script_src/model/supabase_config.js
@@ -1,9 +1,9 @@
 
 // NO SETINS DO PROJETO 
 //Data API:
-const supabaseUrl = 'https://ewbymweduemrufejwzwh.supabase.co';
+const supabaseUrl = 'https://zxkaxteprwxijriycvdx.supabase.co/rest/v1/';
 // Legacy anon, service_role API keys:
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV3Ynltd2VkdWVtcnVmZWp3endoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIzMDg2NjUsImV4cCI6MjA5Nzg4NDY2NX0.5UP1OJg9INBoLL6STSe-Gh33N7Gmt0B7bcU-mgwpFIw';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp4a2F4dGVwcnd4aWpyaXljdmR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI0ODIxMTEsImV4cCI6MjA5ODA1ODExMX0.ffOiXvjJujp4eL2scf1NiMT9A0CtwIAHilbX3T_AMrc';
 // Inicializa o cliente Supabase
 const _supabase = supabase.createClient(supabaseUrl, supabaseKey);
 // Exporta para ser usado em outros scripts
