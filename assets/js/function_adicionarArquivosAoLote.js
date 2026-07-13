6da8c697d21d85f241939b5f4a48a4706f98be46
Aristidesbp <aristidesbp@gmail.com>
2026-07-13 10:13:29 -0300

    Delete assets/js/function_adicionarArquivosAoLote.js

--- assets/js/function_adicionarArquivosAoLote.js
+++
@@ -1,6 +0,0 @@
-
-function adicionarArquivosAoLote(novosArquivos) {
-    // Transforma a lista de novos arquivos num Array e junta com os que já estavam
-    arquivosLote = [...arquivosLote, ...Array.from(novosArquivos)];
-    atualizarListaArquivosLote();
-}
