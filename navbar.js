3941dce96b419c9775b319166812991970423b91
Aristidesbp <aristidesbp@gmail.com>
2026-08-05 10:39:35 -0300

    Update and rename erp_web/html/navbar.js to navbar.js

--- navbar.js
+++ navbar.js
@@ -1,14 +1,4 @@
-// =================================================================
-// 1. CONEXÃO COM O SUPABASE
-// =================================================================
-const supabaseUrl = 'https://mvxwxpwgoukhinqfuppz.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im12eHd4cHdnb3VraGlucWZ1cHB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMwMDE5MzAsImV4cCI6MjA5ODU3NzkzMH0.vudMl-45gMMEg6EJpM8BZa0rC6k7YiAdqtxuUUB_OWM';
 
-// Instância global do cliente Supabase
-const _supabase = supabase.createClient(supabaseUrl, supabaseKey);
-const est_supabase = _supabase; // Compatibilidade com módulo de estoque
-
-let usuarioLogadoId = null;
 
 // =================================================================
 // 2. VERIFICAÇÃO DE LOGIN E SESSÃO
