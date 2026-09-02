f7ef696bf572d2878b8bf31a3fa55562ff5fd5db
Aristidesbp <aristidesbp@gmail.com>
2026-09-02 16:54:40 -0300

    Add files via upload

---
+++ supabase_config.js
@@ -0,0 +1,4 @@
+ /* ================= CONFIGURAÇÕES INICIAIS ================= */
+        const SUPABASE_URL = 'https://ctdkeqltveymtqmtspyu.supabase.co';
+        const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0ZGtlcWx0dmV5bXRxbXRzcHl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgzMjk1NTUsImV4cCI6MjEwMzkwNTU1NX0.ka_jrnDtrOhfPr7_TAl63Z7nyRZeocy2rrkHSLQBOe4';
+        const clienteSupabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
