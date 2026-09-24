a303c9bc9b918e85e98c83c2c345a4252913af49
Aristidesbp <aristidesbp@gmail.com>
2026-09-24 08:10:06 -0300

    Update supabase_config.js

--- supabase_config.js
+++ supabase_config.js
@@ -2,10 +2,10 @@
 // [INÍCIO: CONFIGURAÇÃO SUPABASE]
 // Marca o início lógico do bloco responsável por fazer a conexão matriz com o banco de dados.
 
-const SUPABASE_URL = 'https://vgjgcjhjpfgnpbwzrhei.supabase.co';
+const SUPABASE_URL = 'https://gxbderrxvplxzwvantkn.supabase.co';
 // Define e trava via declaração constante (const) a URL do projeto. Este é o servidor destino de todas as requisições, a porta de acesso ao Supabase.
 
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZnamdjamhqcGZnbnBid3pyaGVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk5NjcyMjksImV4cCI6MjEwNTU0MzIyOX0.MVzECEI_-ZSX1dgLTUt_pvzpUb_eAMi1G3MdubLj56s';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd4YmRlcnJ4dnBseHp3dmFudGtuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAyMTA2MjcsImV4cCI6MjEwNTc4NjYyN30.3YbSndiN-OBFZcOish7MWOu9sO6byPsWDQV74_XbjC4';
 // Define a chave criptográfica anônima/pública. Ela apenas diz "eu sei onde fica o projeto", sem conceder permissões absolutas administrativas.
 
 const clienteSupabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
